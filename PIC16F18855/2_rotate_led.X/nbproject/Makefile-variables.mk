81ccc7121965cf25f3aed61459c08afb5650bdb1
Ruiz Esparza Rivero <mruizes@Chamberlain.com>
2021-06-29 13:37:27 -0700

    add new examples

--- PIC16F18855/2_rotate_led.X/nbproject/Makefile-variables.mk
+++ PIC16F18855/2_rotate_led.X/nbproject/Makefile-variables.mk
@@ -6,8 +6,8 @@
 CND_BASEDIR=`pwd`
 # default configuration
 CND_ARTIFACT_DIR_default=dist/default/production
-CND_ARTIFACT_NAME_default=rotate_led.X.production.hex
-CND_ARTIFACT_PATH_default=dist/default/production/rotate_led.X.production.hex
+CND_ARTIFACT_NAME_default=2_rotate_led.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/2_rotate_led.X.production.hex
 CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
-CND_PACKAGE_NAME_default=rotateled.x.tar
-CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/rotateled.x.tar
+CND_PACKAGE_NAME_default=2rotateled.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/2rotateled.x.tar
